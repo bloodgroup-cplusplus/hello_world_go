238215d3fd91ac7fb1affc61858278d1ec022d1d
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-10-25 14:33:51 +0530

    error handling

--- main.go
+++ main.go
@@ -7,5 +7,9 @@ import (
 
 func main() {
 	args := os.Args
+	if len(args) < 2 {
+		fmt.Printf("Usage ./hello-world <argument>\n")
+		os.Exit(1)
+	}
 	fmt.Printf("Command line arguments: %v\n", args[1:])
 }
