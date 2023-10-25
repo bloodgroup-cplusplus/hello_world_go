37db1a75beb6faac7b37b57347005418e5604471
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-10-25 14:51:20 +0530

    adding url parser

--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 )
 
 func main() {
 	args := os.Args
 	if len(args) < 2 {
-		fmt.Printf("Usage ./hello-world <argument>\n")
+		fmt.Printf("Usage ./http-get <url>\n")
 		os.Exit(1)
 	}
 	fmt.Printf("Command line arguments: %v\n", args[1:])
+
+	if _, err := url.ParseRequestURI(args[1]); err != nil {
+		fmt.Printf("URL is in invalid format: %s\n", err)
+		os.Exit(1)
+	}
+
 }
